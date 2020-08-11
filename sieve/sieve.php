398b4277f08ae3f69a63bf39917d82e487480180
Joshua Peavler <joshuaapeavler@gmail.com>
2020-08-11 16:51:57 -0600

    Completed sieve exercism in PHP

--- sieve/sieve.php
+++ sieve/sieve.php
@@ -1,1 +1,15 @@
 <?php
+
+function sieve($num): array {
+    $primeArr = array_fill(0, $num + 1, true);
+    $primeArr[0] = false;
+    $primeArr[1] = false;
+    for($i = 2; $i < ceil(sqrt($num)); $i++ ) {
+        if($primeArr[$i]) {
+            for($j = $i*$i; $j <= $num; $j += $i) {
+                $primeArr[$j] = false;
+            }
+        }
+    }
+    return array_keys($primeArr, true);
+}
