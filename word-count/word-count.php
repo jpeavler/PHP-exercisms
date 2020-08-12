77fc7a96b9a38c6be34af90d2874f9af04f9adbe
Joshua Peavler <joshuaapeavler@gmail.com>
2020-08-12 10:40:19 -0600

    Completed exercism without using array_count_values function

--- word-count/word-count.php
+++ word-count/word-count.php
@@ -1,1 +1,19 @@
 <?php
+
+function wordCount($phrase): array {
+    $noPunctuation = preg_replace("/[^a-zA-Z0-9\s]/", "", $phrase);
+    $singleSpaceOnly = preg_replace("/\s+/", " ", $noPunctuation);
+    $trimmed = trim($singleSpaceOnly);
+    $phraseArr = explode(" ", strtolower($trimmed));
+    //return array_count_values($phraseArr);   Cheater way for exercism, but should use 
+    //array_count_values function in regular cases
+    $wordCount = array();
+    foreach($phraseArr as $word) {
+        if(isset($wordCount[$word])) {
+            $wordCount[$word] += 1;
+        } else {
+            $wordCount[$word] = 1;
+        }
+    }
+    return $wordCount;
+} 
